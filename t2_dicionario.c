32f1bd96206a513fbe46f491cd6969c0b07b35be
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2017-11-03 22:10:49 -0200

    Update t2_dicionario.c

--- t2_dicionario.c
+++ t2_dicionario.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
+
+typedef struct _in{
+	char op, word[50], def[140];
+}in;
 
 typedef struct _node{
 	char word[50], def[140];
@@ -8,8 +13,8 @@ typedef struct _node{
 }node;
 
 /*
-Em Skip lists eh comum definir o primeiro elemento como -inf e coloca-lo em todos os niveis, como estamos lidando com caracteres podemos definir -inf como 0 (zero) sem perda de generalidade
-*/
+   Em Skip lists eh comum definir o primeiro elemento como -inf e coloca-lo em todos os niveis, como estamos lidando com caracteres podemos definir -inf como 0 (zero) sem perda de generalidade
+   */
 
 void clearNode(node *n){
 	int i = 0;
@@ -25,27 +30,94 @@ void clearNode(node *n){
 	n->r = NULL;
 }
 
+node *searchSL(node *high, char word[50], int lvl, int maxlvlcreated){
+	if((strcmp(high->word, word) == 0) && (lvl == maxlvlcreated))
+		return high;
+	else if((strcmp(high->word, word) == 0) && (lvl != maxlvlcreated))
+		return searchSL(high->d, word, ++lvl, maxlvlcreated);
+
+	else if((strcmp(high->word, word) < 0) && (high->r != NULL))
+		return searchSL(high->r, word, lvl, maxlvlcreated);
+	else if((strcmp(high->word, word) < 0) && (lvl != maxlvlcreated) && (high->r == NULL))
+		return searchSL(high->d, word, ++lvl, maxlvlcreated);
+	else if((strcmp(high->word, word) < 0) && (lvl == maxlvlcreated) && (high->r == NULL))
+		return high;
+
+	else if((strcmp(high->word, word) > 0) && (lvl != maxlvlcreated) && (high->l->word != ""))
+		return searchSL(high->l->d, word, ++lvl, maxlvlcreated);
+	else if((strcmp(high->word, word) > 0) && (lvl != maxlvlcreated) && (high->l->word == ""))
+		return searchSL(high->d, word, ++lvl, maxlvlcreated);
+	else if((strcmp(high->word, word) > 0) && (lvl == maxlvlcreated) && (high->l->word != ""))
+		return searchSL(high->l, word, lvl, maxlvlcreated);
+	else if((strcmp(high->word, word) > 0) && (lvl == maxlvlcreated) && (high->l->word == ""))
+		return high->l;
+
+	return high;
+}
+
+void insertSL(node **high, char word[50], char def[140], int *maxlvlcreated){
+	node *a = searchSL(*high, word, 0, *maxlvlcreated);
+	printf("%p\n", (void *)a);
+	if(strcmp(a->word, word) == 0)
+		printf("OPERCAO INVALIDA\n");
+	else{
+		node *newn;
+		newn = (node *)malloc(sizeof(node));
+		clearNode(newn);
+		strcpy(newn->word, word);
+		strcpy(newn->def, def);
+		newn->l = a;
+		newn->r = a->r;
+		a->r = newn;
+
+		time_t t;
+		srand((unsigned)time(&t));
+		printf("%d\n", rand());
+		if(rand()%2){
+
+		}
+	}
+}
+
 int main(){
-	char c, op, *buffer, def[140];
-	int spc = 0, ondepor = 0;
-	node zero, in;
+	char c, op, *buffer, word[50] = {""}, def[140] = {""};
+	int spc = 0, i = 0, maxlvlcreated = 0;
+	node zero, *high;
 	clearNode(&zero);
-	clearNode(&in);
+	high = &zero;
 
 	while(c = getchar()){
 		if(c == EOF || c == '\n'){
 			if(spc == 1){
 				spc = 0;
-				printf("word %s\n", buffer);
-				//node.def = buffer;
+				strcpy(word, buffer);
+				printf("word %s\n", word);
 			}
 			else if(spc > 2){
 				spc = 0;
 				strcat(def, " ");
 				strcat(def, buffer);
-				printf("fimdef %s\n", def);
+				printf("def %s\n", def);
 				def[0] = '\0';
-				//node.def = buffer;
+			}
+			if(op == 'n'){//iNsercao
+				insertSL(&high, word, def, &maxlvlcreated);
+			}
+			else if(op == 'l'){//aLteracao
+			}
+			else if(op == 'e'){//rEmocao
+			}
+			else if(op == 'u'){//bUsca
+				node *a = searchSL(high, word, 0, maxlvlcreated);
+				if(strcmp(a->word, word) != 0)
+					printf("OPERCAO INVALIDA\n");
+				else
+					printf("%s %s\n", a->word, a->def);
+			}
+			else if(op == 'm'){//iMpressao
+			}
+			else{
+				printf("OPERCAO INVALIDA\n");
 			}
 			if(c == EOF)
 				break;
@@ -59,12 +131,14 @@ int main(){
 				printf("op %c\n", op);
 			}
 			else if(spc == 2){
-				printf("word %s\n", buffer);
-				//node.word = buffer;
+				strcpy(word, buffer);
+				printf("word %s\n", word);
 			}
-			else if(spc > 2){
-				if(spc > 3)
-					strcat(def, " ");
+			else if(spc == 3){
+				strcpy(def, buffer);
+			}
+			else{
+				strcat(def, " ");
 				strcat(def, buffer);
 			}
 		}
